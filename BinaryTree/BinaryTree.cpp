59f57e81a2225b881f266a5d67e777d1e3d831f2
karina <karinaanton77@gmail.com>
2019-11-12 00:51:56 +0300

    added 6 task

---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Карина Зайнуллина on 11/11/2019.
+//
+
+
+#include "BinaryTree.h"
+
+void BinaryTree::addLeave(int value) {
+    Node *node = new Node(value);
+    Node *currentNode = root;
+    Node *prevNode = nullptr;
+    while (currentNode != nullptr) {
+        prevNode = currentNode;
+        if (currentNode->getValue() > value) {
+            currentNode = currentNode->getLeftChild();
+        } else {
+            currentNode = currentNode->getRightChild();
+        }
+    }
+
+    if (prevNode == nullptr) {
+        root = node;
+    } else if (prevNode->getValue() > value) {
+        prevNode->setLeftChild(node);
+    } else {
+        prevNode->setRightChild(node);
+    }
+}
+
+std::string BinaryTree::inOrderTraversal() {
+    {
+        std::stack<Node> stack;
+        Node *currentNode = root;
+        std::string str;
+
+        while (currentNode != nullptr || !stack.empty()) {
+            if (currentNode != nullptr) {
+                stack.push(*currentNode);
+                currentNode = currentNode->getLeftChild();
+            } else {
+                currentNode = &stack.top();
+                stack.pop();
+                str += std::to_string(currentNode->getValue()) + " ";
+                currentNode = currentNode->getRightChild();
+            }
+        }
+        return str;
+    }
+}
+
+int binaryTreeMain() {
+    BinaryTree binaryTree = BinaryTree();
+    int size;
+    std::cin >> size;
+    int numberToAdd;
+    for (int i = 0; i < size; ++i) {
+        std::cin >> numberToAdd;
+        binaryTree.addLeave(numberToAdd);
+    }
+
+    std::cout << binaryTree.inOrderTraversal();
+}
---
+++ BinaryTree/BinaryTree.h
@@ -0,0 +1,50 @@
+//
+// Created by Карина Зайнуллина on 11/11/2019.
+//
+
+#ifndef MADE_CPP_BINARYTREE_H
+#define MADE_CPP_BINARYTREE_H
+
+#include <iostream>
+#include <stack>
+
+class BinaryTree {
+private:
+    class Node {
+    private:
+        Node *leftChild = nullptr;
+        Node *rightChild = nullptr;
+        int value;
+    public:
+        explicit Node(int value) : value(value) {}
+
+        int getValue() { return value; }
+
+        Node *getLeftChild() const {
+            return leftChild;
+        }
+
+        Node *getRightChild() const {
+            return rightChild;
+        }
+
+        void setLeftChild(Node *leftChild) {
+            Node::leftChild = leftChild;
+        }
+
+        void setRightChild(Node *rightChild) {
+            Node::rightChild = rightChild;
+        }
+
+    };
+
+
+    Node *root = nullptr;
+
+public:
+    void addLeave(int value);
+
+    std::string inOrderTraversal();
+};
+
+#endif //MADE_CPP_BINARYTREE_H
